c598af226f687803163f6c1235b51bf2eea0da70
fl111x <ammar.bfw@gmail.com>
2024-07-28 16:13:42 +0700

    latihan node js vid 7

--- runFileNode/function.js
+++ runFileNode/function.js
@@ -2,4 +2,15 @@ function sayHello(nama,umur) {
     return `hallo nama saya ${nama}, dan umur saya adalah ${umur}`;
 }
 
-module.exports = sayHello;
+const kelas = 13;
+const mahasiswa = {
+    nama:`Ammar`,
+    kelas:`A`,
+    golonganDarah:`AB`,
+    sapa() {
+        return `halo nama saya ${this.nama}, saya kelas ${this.kelas}, dan golongan darah ${this.golonganDarah}`;
+    }
+}
+
+
+module.exports = {sayHello,kelas,mahasiswa};
